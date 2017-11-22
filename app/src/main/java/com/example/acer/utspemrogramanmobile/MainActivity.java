b68cdb442a7c6de475bcc9760548d89880edb39c
Irfan Daeng <mirfandaeng@gmail.com>
2017-11-22 11:27:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/acer/utspemrogramanmobile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.acer.utspemrogramanmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
