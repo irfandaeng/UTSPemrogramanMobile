6b2c24bc8ce73fb633657ab4bde4d2517816d965
Irfan Daeng <mirfandaeng@gmail.com>
2017-11-22 12:24:30 +0700

    1

--- app/src/main/java/com/example/acer/utspemrogramanmobile/MainActivity.java
+++ app/src/main/java/com/example/acer/utspemrogramanmobile/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acer.utspemrogramanmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.acer.utspemrogramanmobile.mData.BangunRuangCollection;
+import com.example.acer.utspemrogramanmobile.mListAdapter.CustomAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list_bangun);
+        CustomAdapter adapter = new CustomAdapter(this, BangunRuangCollection.getBangunRuang());
+        listView.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/acer/utspemrogramanmobile/mListAdapter/CustomAdapter.java
+++ app/src/main/java/com/example/acer/utspemrogramanmobile/mListAdapter/CustomAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.acer.utspemrogramanmobile.R;
 import com.example.acer.utspemrogramanmobile.mData.BangunRuang;
 
 import java.util.ArrayList;
@@ -44,11 +47,27 @@ public class CustomAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        if (convertView = null)
+        if (convertView == null)
         {
-            convertView = inflater.inflate();
+            convertView = inflater.inflate(R.layout.custom_list_view,parent,false);
         }
 
-        return null;
+        TextView judHome = (TextView)convertView.findViewById(R.id.namaHome);
+        ImageView imgHome = (ImageView)convertView.findViewById(R.id.imgHome);
+
+        final String judul = bangunRuangs.get(position).getNamaBangun();
+        final int gambar = bangunRuangs.get(position).getImageBangun();
+
+        judHome.setText(judul);
+        imgHome.setImageResource(gambar);
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return convertView;
     }
 }
